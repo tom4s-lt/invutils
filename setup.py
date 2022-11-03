20425ffdbbcd374dd8bb3285289e088f191dbcdf
xtom4s <71655945+xtom4s@users.noreply.github.com>
2022-11-03 18:09:41 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,4 +16,4 @@ setup(name="inverutils",
 	url="https://github.com/xtom4s/inverutilities",
 	keywords="" ,
 	classifiers=["Programming Language :: Python :: 3","License :: OSI Approved :: MIT License","Operating System :: OS Independent"],
-	install_requires=["pandas","datetime","requests"])
+	install_requires=["requests", "pandas", "datetime", "time"])
