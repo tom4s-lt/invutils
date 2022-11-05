b1afb9cc7d15ad8a45fd76a9cc8c308b1dc36a26
xtom4s <71655945+xtom4s@users.noreply.github.com>
2022-11-05 09:13:40 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
 from setuptools import setup, find_packages
 from os import path
 
-# this_directory = path.abspath(path.dirname(__file__))
-# with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-#     long_description = f.read()
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(name="inverutils",
 	packages=find_packages() ,
 	version="1.0.1", 
-	descrription="Tools for investing portfolio", 
+	descrription="Tools for tracking investing portfolio", 
 	long_description = long_description,
 	long_description_content_type = "text/markdown",
 	author="Tomas Juan Galizia",
 	author_email="xtom4s@gmail.com", 
 	url="https://github.com/xtom4s/inverutilities",
-	keywords="",
 	classifiers=["Programming Language :: Python :: 3","License :: OSI Approved :: MIT License","Operating System :: OS Independent"],
 	install_requires=["requests", "pandas", "datetime"]) # For some reason it fails with "time" inside, so it is removed as it is a built-in module
