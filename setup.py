f0603206046fb2a1bdfa230266bf80cee8527b5c
xtom4s <71655945+xtom4s@users.noreply.github.com>
2022-11-05 09:14:30 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,9 @@
 from setuptools import setup, find_packages
-from os import path
-
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
 
 setup(name="inverutils",
 	packages=find_packages() ,
 	version="1.0.1", 
 	descrription="Tools for tracking investing portfolio", 
-	long_description = long_description,
 	long_description_content_type = "text/markdown",
 	author="Tomas Juan Galizia",
 	author_email="xtom4s@gmail.com", 
