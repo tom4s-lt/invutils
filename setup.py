68b504219deb374ed6b728f3d6c996f4f51b25ab
xtom4s <71655945+xtom4s@users.noreply.github.com>
2022-11-05 09:02:37 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,6 +14,6 @@ setup(name="inverutils",
 	author="Tomas Juan Galizia",
 	author_email="xtom4s@gmail.com", 
 	url="https://github.com/xtom4s/inverutilities",
-	keywords="" ,
+	keywords="",
 	classifiers=["Programming Language :: Python :: 3","License :: OSI Approved :: MIT License","Operating System :: OS Independent"],
 	install_requires=["requests", "pandas", "datetime"]) # For some reason it fails with "time" inside, so it is removed as it is a built-in module
