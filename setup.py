34e913f50c70b3bfcfe1b5f1e796a8412b4c4ab1
xtom4s <71655945+xtom4s@users.noreply.github.com>
2022-12-02 11:12:47 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,25 @@
 from setuptools import setup, find_packages
 
-setup(name="inverutils",
-	packages=find_packages() ,
-	version="1.0.1", 
-	description="Tools for tracking investing portfolio", 
-	author="Tomas Juan Galizia",
-	author_email="xtom4s@gmail.com", 
-	url="https://github.com/xtom4s/inverutilities",
-	install_requires=["requests", "pandas", "datetime"]) # For some reason it fails with "time" inside, so it is removed as it is a built-in module
+setup(name = "invutils",
+      description = "Tools aimed at investing and portfolio tracking, mainly for cryptocurrency",
+      version = "1.0.1",
+      url = "https://github.com/xtom4s/inverutilities",
+      license = "MIT License",
+      author = "Tom4s",
+      author_email = "tom4s.rr@gmail.com",
+      packages = find_packages(),
+      install_requires = [  # For some reason it fails with "time" inside, so it is removed as it is a built-in module
+	      "requests",
+	      "pandas",
+	      "datetime",
+	      "time"
+	      "base64",
+      	],
+      classifiers = [
+	      "Programming Language :: Python",
+	      "Development Status :: 1 - Beta",
+	      "Intended Audience :: Anyone who knows python",
+	      "License :: OSI Approved :: MIT License",
+	      "Operating System :: OS Independent"
+      	]
+)
