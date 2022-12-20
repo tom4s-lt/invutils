1e8e9155c8bdf99b2c6f5650717648264a1426d5
xtom4s <71655945+xtom4s@users.noreply.github.com>
2022-12-20 19:22:53 -0300

    Update px_reqs.py

--- invutils/px_reqs.py
+++ invutils/px_reqs.py
@@ -1,6 +1,9 @@
 """Function definitions for price requests to various APIs
 """
 
+import util.helpers as hl
+from constants import *
+
 import requests
 import pandas as pd
 from datetime import datetime
@@ -28,7 +31,7 @@ def gecko_current(id_gecko:str, vs_currencies:str = 'usd'):
   assert type(id_gecko) is str, 'id_gecko should be a str'
   assert type(vs_currencies) is str, 'vs_currencies should be a str'
 
-  url = "https://api.coingecko.com/api/v3/simple/price"
+  url = COINGECKO_ENDPOINTS['px_current']
   
   try:
     res = requests.get(url, params = {
@@ -64,7 +67,7 @@ def gecko_hist(id_gecko:str, vs_currency:str = 'usd', days = 'max'):
   assert type(id_gecko) is str, 'id_gecko should be a str'
   assert type(vs_currency) is str, 'vs_currency should be a str'
   
-  url = 'https://api.coingecko.com/api/v3' + f'/coins/{id_gecko}/market_chart'
+  url = COINGECKO_ENDPOINTS['px_hist'] % (id_gecko)
   
   try:
     res = requests.get(url, params = {
@@ -108,8 +111,8 @@ def llama_hist(id_llama:str, timestamp = int(time.mktime(datetime.now().timetupl
   """
   assert type(id_llama) is str, 'id_llama should be a str'
   assert type(timestamp) is int, 'timestamp should be an int representing unix timestamp'
-
-  url = f"https://coins.llama.fi/prices/historical/{timestamp}/{id_llama}"
+  
+  url = DEFILLAMA_ENDPOINTS['px_hist'] % (timestamp, id_llama)
 
   try:    
     res = requests.get(url)
@@ -127,50 +130,6 @@ def llama_hist(id_llama:str, timestamp = int(time.mktime(datetime.now().timetupl
 
     return df
   
-  except requests.exceptions.HTTPError as errh:
-    print("Http Error:", errh)
-
-
-# ------------------------------------------------------------------------------
-# Zapper
-# ------------------------------------------------------------------------------
-
-def zapper_current_network(credentials:str, network:str):
-  """Zapper - Get current prices for all tokens supported in zapper - for a given network
-  If bad credentials passed - returns HTTP error on bad auth
-  If bad network is passed - returns HTTP error on bad request
-
-  Args:
-    credentials (str): zapper api_key (personal)
-    network (str): desired network for token price search (e.g. ethereum, arbitrum, optimism)
-  
-  Returns:
-    df (DataFrame): df containing - index -> [date(%Y-%m-%d)], \
-      columns -> [address, name, symbol, coingeckoId, price, network], values -> [described in cols]
-  """
-  assert type(credentials) is str, 'credentials should be a str'
-  assert type(network) is str, 'network should be a str'
-  
-  credentials = credentials + ":"
-  encodedBytes = base64.b64encode(credentials.encode("utf-8")) # https://www.base64encoder.io/python/
-  encodedStr = str(encodedBytes, "utf-8")
-
-  url = "https://api.zapper.fi/v2/prices"
-  
-  try:
-    res = requests.get(url,
-                params = {'network': network},
-                headers = {'Authorization': f"Basic {encodedStr}"}
-                )
-    res.raise_for_status()
-
-    df = pd.DataFrame.from_records(res.json())
-    df = df[['address', 'name', 'symbol', 'coingeckoId', 'price', 'network']]
-    df.loc[:, 'date'] = pd.to_datetime(datetime.now().strftime("%Y/%m/%d"))
-    df.set_index('date', inplace = True)
-  
-    return df
-
   except requests.exceptions.HTTPError as errh:
     print("Http Error:", errh)
 
@@ -196,7 +155,7 @@ def cmc_current(credentials:str, id_cmc:str):
   assert type(id_cmc) is str, 'id_cmc should be a str'
 
   # Pass slugs here because in params, requests converts them to 'bitcoin%2Cethereum' and cmc api takes 'bitcoin,ethereum'
-  url = f"https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?slug={id_cmc}"
+  url = COINMARKETCAP_ENDPOINTS['px_current'] + f"?slug={id_cmc}"
 
   try:
     res = requests.get(url,
@@ -220,6 +179,117 @@ def cmc_current(credentials:str, id_cmc:str):
 
 
 # ------------------------------------------------------------------------------
+# Zapper
+# ------------------------------------------------------------------------------
+
+def zapper_current_network(credentials:str, network:str):
+  """Zapper - Get current prices for all tokens supported in zapper - for a given network
+  If bad credentials passed - returns HTTP error on bad auth
+  If bad network is passed - returns HTTP error on bad request
+
+  Args:
+    credentials (str): zapper api_key (personal)
+    network (str): desired zapper network id for token price search (e.g. ethereum, arbitrum, optimism)
+  
+  Returns:
+    df (DataFrame): df containing - index -> [date(%Y-%m-%d)], \
+      columns -> [address, name, symbol, coingeckoId, price, network], values -> [described in cols]
+  """
+  assert type(credentials) is str, 'credentials should be a str'
+  assert type(network) is str, 'network should be a str'
+  
+  credentials = credentials + ":"
+  encodedBytes = base64.b64encode(credentials.encode("utf-8")) # https://www.base64encoder.io/python/
+  encodedStr = str(encodedBytes, "utf-8")
+
+  url = ZAPPER_ENDPOINTS['px_current']
+  
+  try:
+    res = requests.get(url,
+                params = {'network': network},
+                headers = {'Authorization': f"Basic {encodedStr}"}
+                )
+    res.raise_for_status()
+
+    df = pd.DataFrame.from_records(res.json())
+    df = df[['address', 'name', 'symbol', 'coingeckoId', 'price', 'network']]
+    df.loc[:, 'date'] = pd.to_datetime(datetime.now().strftime("%Y/%m/%d"))
+    df.set_index('date', inplace = True)
+  
+    return df
+
+  except requests.exceptions.HTTPError as errh:
+    print("Http Error:", errh)
+
+
+# ------------------------------------------------------------------------------
 # Etherscan
 # ------------------------------------------------------------------------------
 
+
+def exp_univ2_current(credentials:str, network:str, pool:str, subj1:str, subj2:str, pool_dec:int = 18, subj1_dec:int = 18, subj2_dec:int = 18):
+  """Get current price of any lp token following uniswap v2 model
+  Currently supports arbitrum only
+  
+  Args:
+    credentials (str): explorer api key
+    network (str): blockchain where the pool is located
+    pool (str): address of the pool - the lp token contract address
+    subj1 (str): token address of subjacent number 1
+    subj2 (str): token address of subjacent number 1
+    pool_dec (int): number of decimals of pool token
+    subj1_dec (int): number of decimals of subj1 token
+    subj2_dec (int): number of decimals of subj2 token
+  
+  Returns:
+    df (DataFrame): timeseries df (one row) containing current px for each asset in columns - index -> [date (%Y-%m-%d)], columns -> [id_cmc], values -> [current price]
+  """
+  assert type(credentials) is str, 'credentials should be a str'
+  assert type(network) is str, 'network should be a str'
+  assert type(pool) is str, 'pool address should be a str'
+  assert type(subj1) is str, 'subj1 address should be a str'
+  assert type(subj2) is str, 'subj2 address should be a str'
+  assert type(pool_dec) is int, 'pool_dec should be a int'
+  assert type(subj1_dec) is int, 'subj1_dec should be a int'
+  assert type(subj2_dec) is int, 'subj2_dec should be a int'
+
+  network = hl.check_chain(network)
+
+  try:
+    url = EXPLORER_ENDPOINTS[NETWORK_INFO[network]['explorer']]
+
+    res = requests.get(url + EXPLORER_PARAMS['ERC_20_TOKEN_SUPPLY'] % (pool, credentials))
+    
+    pool_info = {}
+    pool_info[pool] = {'balance': int(res.json()['result']) / 10**pool_dec} # # could be improved for use without univ2 model
+    
+    subj_dec = {subj1: subj1_dec, subj2: subj2_dec}
+    
+    subj_prices = prq.llama_hist(f"{network}:{subj1},{network}:{subj2}")
+    
+    for subj in subj_dec:
+
+      res = requests.get(url + EXPLORER_PARAMS['ERC_20_TOKEN_BALANCE'] % (subj, pool, credentials))
+
+      pool_info[subj] = {
+          'balance': int(res.json()['result']) / 10**subj_dec[subj],
+          'price': subj_prices[f"{network}:{subj}"].iloc[0]
+      }
+    
+    # This 0 is for tidiness in format
+    pool_info[pool]['price'] = (0 \
+    + pool_info[subj1]['balance'] * pool_info[subj1]['price'] \
+    + pool_info[subj1]['balance'] * pool_info[subj1]['price']) \
+    / pool_info[pool]['balance']
+
+    current_date = pd.to_datetime(datetime.now().strftime("%Y-%m-%d"))
+
+    df = pd.DataFrame(pool_info[pool]['price'], columns = [f"{network}:{pool}"], index = current_date)
+
+    return df
+  
+  except KeyError:
+    print("Chain explorer not supported:", network)
+  
+  except requests.exceptions.HTTPError as errh:
+    print("Http Error:", errh)
