aeb32f2a2260b9eb5dbdd3705673a54cede7f541
tom4s-lt <tom4s.rr@gmail.com>
2025-11-28 20:36:28 -0300

    correct max time horizon for coingecko historical prices
    demo account only allows for 365 so default is now 365

--- invutils/prices/coingecko.py
+++ invutils/prices/coingecko.py
@@ -55,7 +55,7 @@ def gecko_price_current(id_gecko: str, vs_currencies: str = 'usd', api_key: Opti
   )
 
 
-def gecko_price_hist(id_gecko: str, vs_currency: str = 'usd', days: Union[int, str] = 'max', api_key: Optional[str] = None) -> Optional[Dict[str, Any]]:
+def gecko_price_hist(id_gecko: str, vs_currency: str = 'usd', days: Union[int, str] = '365', api_key: Optional[str] = None) -> Optional[Dict[str, Any]]:
   """
   CoinGecko - Get CoinGecko historical price data of coin.
   If bad id_gecko is passed - returns HTTP error
